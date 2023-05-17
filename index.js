e1907c44792e3152b082ecd455b9216811cca6c9
Paul Lai <plutodark@gmail.com>
2023-05-17 23:55:43 +0100

    start serverless

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports.handler = async (event) => {
+  console.log('meow')
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v3.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
